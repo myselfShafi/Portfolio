40a72c5c85c4ea9355eee241b72698d0201898c7
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-02-17 15:13:47 +0530

    background edits

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -5,11 +5,9 @@ import "./navbar.style.css";
 const Navbar = () => {
   return (
     <nav className="nav-body">
-      <div className="head-div">
-        <h3 children={"+ 91 9666 370 688"} className="head-tag" />
+      <h3 children={"+ 91 9666 370 688"} className="head-tag" />
 
-        <h3 children={"iamshafi25@gmail.com"} className="head-tag" />
-      </div>
+      <h3 children={"iamshafi25@gmail.com"} className="head-tag" />
     </nav>
   );
 };
