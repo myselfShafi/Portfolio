0b776c19b0ff02996d0de67eba52bbe02efe4b96
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-02-17 21:56:33 +0530

    preloader setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import { About } from "./components/About/about";
 import { Contact } from "./components/Contact/contact";
@@ -6,20 +7,38 @@ import { Experience } from "./components/Experience/experience";
 import { Footer } from "./components/Footer/footer";
 import { Homepage } from "./components/Homepage/homepage";
 import Navbar from "./components/Navbar/navbar";
+import { Preloader } from "./components/Preloader/preloader";
 import Sidebar from "./components/Sidebar/sidebar";
 
 function App() {
+  useEffect(() => {
+    var loader = document.querySelector(".preloader");
+    var content = document.querySelector(".App");
+
+    if (loader && content) {
+      window.addEventListener("load", () => {
+        setTimeout(() => {
+          loader.classList.add("preloader-hidden");
+          content.classList.add("App-visible");
+        }, 2000);
+      });
+    }
+  }, []);
+
   return (
-    <div id="home" className={`App`}>
-      <Navbar />
-      <Sidebar />
-      <Homepage />
-      <About />
-      <Experience />
-      <EduSkill />
-      <Contact />
-      <Footer />
-    </div>
+    <React.Fragment>
+      <Preloader />
+      <div id="home" className={`App`}>
+        <Navbar />
+        <Sidebar />
+        <Homepage />
+        <About />
+        <Experience />
+        <EduSkill />
+        <Contact />
+        <Footer />
+      </div>
+    </React.Fragment>
   );
 }
 
