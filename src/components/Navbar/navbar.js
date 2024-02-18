24ce34164e446ae8912c1419f43f8fbdaa9c0b02
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-02-18 13:32:33 +0530

    navbar elements

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import { GiAsianLantern } from "react-icons/gi";
 import "./navbar.style.css";
 
 const Navbar = () => {
   return (
     <nav className="nav-body">
       <h3 children={"+ 91 9666 370 688"} className="head-tag" />
-
+      <GiAsianLantern size={"30px"} className="lantern" />
       <h3 children={"iamshafi25@gmail.com"} className="head-tag" />
     </nav>
   );
